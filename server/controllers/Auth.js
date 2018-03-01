a7744bd38673ad409408a7232e543d0dc670333b
stellastella <wuchukwustella333@gmail.com>
2018-03-01 11:03:36 +0100

    initialize sequelize

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,12 +1,12 @@
 class Auth {
-    static login (req, res) {
-      // console.log(req.body);
-      const firstName = req.body.first_name;
-      const lastName = req.body.last_name;
-      const email = req.body.email;
-      const password = req.body.password;
-      res.send({firstName: firstName, lastName: lastName, email: email, password: password});
-    }
+  static login (req, res) {
+    console.log(req.body);
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    res.send({firstName: firstName, lastName: lastName, email: email, password: password});
   }
-  
-  module.exports = Auth;
+}
+
+module.exports = Auth;
